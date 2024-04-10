4c742d8efa9f9aab5a30d5d3a08a330c445255c8
EmilioRivera0 <elcholula619@gmail.com>
2024-04-10 00:14:52 -0600

    /products endpoints implemented

--- backend_challenge/app.py
+++ backend_challenge/app.py
@@ -2,6 +2,7 @@
 from sqlalchemy import Column, Integer, String, Double, Date, ForeignKey, create_engine, select, update, delete
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 from chalice import Chalice, Response
+import sqlalchemy.exc
 from json import dumps
 import psycopg2
 import os
@@ -72,6 +73,7 @@ def unit_measure_endpoint():
         with Session() as session:
             session.add(um)
             session.commit()
+
         return Response("UnitMeasure created successfully", status_code=201)
     
     # get
@@ -94,6 +96,7 @@ def unit_measure_endpoint():
         if result == None:
             return Response("Element not found", status_code=404)
         result = dumps({"id": result[0].id, "name": result[0].name})
+
         return Response(result, status_code=200)
 
     # update
@@ -112,6 +115,7 @@ def unit_measure_endpoint():
             # update the row
             session.execute(update(UnitMeasures).where(UnitMeasures.id == body['id']).values(name=body['name']))
             session.commit()
+        
         return Response("UnitMeasure updated successfully", status_code=201)
     
     # delete
@@ -121,7 +125,7 @@ def unit_measure_endpoint():
             return Response('No body provided', status_code=406)
         # verify if the required elements are present in the body json
         if not 'id' in body:
-            return Response("Body needs 'id'element", status_code=406)
+            return Response("Body needs 'id' element", status_code=406)
         # delete specified row
         with Session() as session:
             # check if row exists in the db
@@ -130,11 +134,103 @@ def unit_measure_endpoint():
             # delete the row
             session.execute(delete(UnitMeasures).where(UnitMeasures.id == body['id']))
             session.commit()
+
         return Response("UnitMeasure deleted successfully", status_code=201)
 
 @app.route('/products', methods=['POST','GET','PUT','DELETE'])
 def products_endpoint():
-    pass
+    # save the json body in a dictionary
+    body = app.current_request.json_body
+
+    # create
+    if app.current_request.method == 'POST':
+        # return error if no json body is provided
+        if body == None or len(body) == 0:
+            return Response('No body provided', status_code=406)
+        # verify if the required elements are present in the body json
+        if not ('name' in body and 'price' in body and 'um_id' in body):
+            return Response("Body needs 'name', 'price' and 'um_id' elements", status_code=406)
+        # initialize UnitMeasure object with body data and add it to the db
+        pr = Products(name=body['name'], price=body['price'], um_id=body['um_id'])
+        # if um_id does not exist, catch IntegrityError and return feedback to the client
+        try:
+            with Session() as session:
+                session.add(pr)
+                session.commit()
+        except sqlalchemy.exc.IntegrityError:
+            return Response("um_id does not exist in the db", status_code=404)
+        # catch any other errors
+        except:
+            return Response("An error occured", status_code=500)
+
+        return Response("Product created successfully", status_code=201)
+    
+    # get
+    elif app.current_request.method == 'GET':
+        # get all rows if no 'name' element is specified in body
+        if body == None or len(body) == 0:
+            with Session() as session:
+                result = session.execute(select(Products)).all()
+            # serialize data into a list of dictionaries and return it
+            result = [{'name': it[0].name, 'price': it[0].price, 'um_id': it[0].um_id} for it in result]
+            result = dumps(result)
+            return Response(result, status_code=200)
+        # check if 'name' element is contained in the body
+        if not 'name' in body:
+            return Response("Body needs 'name' element", status_code=406)
+        # get the specified row
+        with Session() as session:
+            result = session.execute(select(Products).where(Products.name == body['name'])).fetchone()
+        # if element does not exist in the db
+        if result == None:
+            return Response("Element not found", status_code=404)
+        result = dumps({'name': result[0].name, 'price': result[0].price, 'um_id': result[0].um_id})
+
+        return Response(result, status_code=200)
+
+    # update
+    elif app.current_request.method == 'PUT':
+        # return error if no json body is provided
+        if body == None or len(body) == 0:
+            return Response('No body provided', status_code=406)
+        # verify if the required elements are present in the body json
+        if not ('name' in body and 'price' in body and 'um_id' in body):
+            return Response("Body needs 'name', 'price' and 'um_id' elements", status_code=406)
+            # update specified row
+        with Session() as session:
+            # check if row exists in the db
+            if session.execute(select(Products).where(Products.name == body['name'])).fetchone() == None:
+                return Response("Element does not exist", status_code=404)
+            # if um_id does not exist, catch IntegrityError and return feedback to the client
+            try:
+                # update the row
+                session.execute(update(Products).where(Products.name == body['name']).values(price=body['price'], um_id=body['um_id']))
+                session.commit()
+            except sqlalchemy.exc.IntegrityError:
+                return Response("um_id does not exist in the db", status_code=404)
+            # catch any other errors
+            except:
+                return Response("An error occured", status_code=500)
+
+        return Response("Product updated successfully", status_code=201)
+    
+    # delete
+    elif app.current_request.method == 'DELETE':
+        # return error if no json body is provided
+        if body == None or len(body) == 0:
+            return Response('No body provided', status_code=406)
+        # verify if the required elements are present in the body json
+        if not 'name' in body:
+            return Response("Body needs 'name' element", status_code=406)
+        # delete specified row
+        with Session() as session:
+            # check if row exists in the db
+            if session.execute(select(Products).where(Products.name == body['name'])).fetchone() == None:
+                return Response("Element does not exist", status_code=404)
+            # delete the row
+            session.execute(delete(Products).where(Products.name == body['name']))
+            session.commit()
+        return Response("Product deleted successfully", status_code=201)
 
 @app.route('/sales', methods=['POST','GET'])
 def sales_endpoint():
